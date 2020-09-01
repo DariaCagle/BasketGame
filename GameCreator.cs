e2b8a9488188fa8a65e9fc2e4d2e8e2d321a2bc2
DariaCagle <dani.merchard@gmail.com>
2020-09-01 13:22:06 +0300

    initial

---
+++ GameCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketGame
+{
+    public class GameCreator
+    {
+        public static List<Gamer> gamers = new List<Gamer>();
+
+        public GameCreator()
+        {
+
+        }
+
+    }
+}
---
+++ Gamer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketGame
+{
+    public class Gamer
+    {
+        public virtual void Move()
+        {
+
+        }
+        public virtual void AddMyNumbers()
+        {
+
+        }
+    }
+}
